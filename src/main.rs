63d901b812aff4419436db1d18fcad487ec46c94
Leikoe <leo.paille@etu.u-bordeaux.fr>
2023-08-19 15:53:24 +0200

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::collections::HashSet;
+use std::fs;
+
+
+fn main() {
+
+    let fs::read_to_string("input.txt").unwrap()
+    let chars  = HashSet::from();
+    println!("Hello, world!");
+}
