52acc4d26f6bd0775b8dfacc2da1e87caf4ed968
Léo Paillé <leo.paille@etu.u-bordeaux.fr>
2023-08-19 18:21:08 +0200

    feat: added rayon

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::time::Instant;
+use miniz_oxide::deflate::compress_to_vec;
+use rayon::prelude::*;
 
 
 fn main() {
-    let text = fs::read_to_string("input.txt").unwrap();
+    let start = Instant::now();
+    let text = fs::read_to_string("input.txt").unwrap()[..5000].to_string();
 
     // # here are all the unique characters that occur in this text
     let chars = {
@@ -14,22 +18,90 @@ fn main() {
     };
 
     // create a mapping from characters to integers
-    // stoi = { ch:i for i,ch in enumerate(chars) }
-    // itos = { i:ch for i,ch in enumerate(chars) }
-    // encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-    // decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
-    // let mut stoi = HashMap::new();
-    let mut stoi : HashMap<char, i64> = HashMap::from_iter(
-        chars.iter().enumerate().map(|(i, c)| (c.clone(), i as i64))
+    let mut stoi : HashMap<char, u8> = HashMap::from_iter(
+        chars.iter().enumerate().map(|(i, c)| (c.clone(), i as u8))
     );
-    let mut itos : HashMap<i64, char> = HashMap::from_iter(
-        chars.iter().enumerate().map(|(i, c)| (i as i64, c.clone()))
+    let mut itos : HashMap<u8, char> = HashMap::from_iter(
+        chars.iter().enumerate().map(|(i, c)| (i as u8, c.clone()))
     );
 
-    let encode = |s: &str| s.chars().map(|c| stoi.get(&c).unwrap().clone()).collect::<Vec<i64>>();
-    let decode = |l: Vec<i64>| l.iter().map(|i| itos.get(i).unwrap()).collect::<String>();
+    // encoder: take a string, output a list of integers
+    let encode = |s: &str| s.chars().map(|c| *stoi.get(&c).unwrap()).collect::<Vec<u8>>();
+    // decoder: take a list of integers, output a string
+    let decode = |l: Vec<u8>| l.iter().map(|i| itos.get(i).unwrap()).collect::<String>();
 
-    dbg!(decode(encode("lol")));
-    println!("Hello, world!");
+    let data = encode(text.as_str());
+    let n_vocab: i64 = chars.len() as i64;
+    let n_ctx : i64 = 8;
+    let n_train: i64 = data.len() as i64;
+
+    println!("n_vocab = {n_vocab}");
+    println!("n_ctx   = {n_ctx}");
+    println!("n_train = {n_train}");
+
+
+    let mut X = Vec::with_capacity((n_train*n_ctx) as usize);
+    let mut Y= Vec::with_capacity((n_train*n_ctx) as usize);
+
+    let before = Instant::now();
+    let d = get_data(&data, n_ctx);
+    println!("get_data | Elapsed time: {:.2?}", before.elapsed());
+
+    let before = Instant::now();
+    for (x, y) in d.0.iter().zip(d.1.iter()) {
+        for token_idx in 0..n_ctx as usize {
+            let context = &x[..token_idx+1];
+            let target = &y[token_idx];
+
+            // println!("when context is {:?}, target is {}", decode(context.to_vec()), itos.get(target).unwrap());
+            X.push((context, compress_to_vec(context, 6).len() as i64));
+            Y.push(target);
+        }
+    }
+    println!("X, Y | Elapsed time: {:.2?}", before.elapsed());
+
+    let before = Instant::now();
+    let ncd_scores = (0..X.len())
+        .into_par_iter()
+        .map(|i| (0..X.len())
+            .map(|j: usize| ncd(X[i].0, X[i].1, X[j].0, X[j].1))
+                .collect::<Vec<f64>>())
+        .collect::<Vec<Vec<f64>>>();
+    println!("ncd_scores | Elapsed time: {:.2?}", before.elapsed());
+
+
+    println!("TOOK: {}", start.elapsed().as_secs_f64());
 }
+
+fn get_data(data: &Vec<u8>, n_ctx: i64) -> (Vec<&[u8]>, Vec<&[u8]>) {
+    // data = train_data if split == 'train' else val_data
+    // ix = np.array(range(0, len(data) - n_ctx))  # TODO: is step a good idea ?
+    // x = np.stack([data[i:i + n_ctx] for i in ix])
+    // y = np.stack([data[i + 1:i + n_ctx + 1] for i in ix])
+
+    let ix = Vec::from_iter(0..(data.len() - n_ctx as usize));
+    let x = ix.iter().map(|&i| &data[i..(i+n_ctx as usize)]).collect();
+    let y = ix.iter().map(|&i| &data[(i+1)..(i+n_ctx as usize+1)]).collect();
+
+    (x, y)
+}
+
+fn concat_with_space(x: &[u8], y: &[u8]) -> Vec<u8> {
+    let mut s = Vec::with_capacity(x.len() + 1 + y.len());
+    for _x in x {
+        s.push(*_x);
+    }
+    s.push(' ' as u8);
+    for _y in y {
+        s.push(*_y);
+    }
+
+    s
+}
+
+fn ncd(x: &[u8], x_compressed: i64, x2: &[u8], x2_compressed: i64) -> f64 {
+    let xx2 = compress_to_vec(&*concat_with_space(x, x2), 6).len() as i64;
+    (xx2 - x_compressed.min(x2_compressed)) as f64 / x_compressed.max(x2_compressed) as f64
+}
+
+
