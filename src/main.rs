105469d224ca62df07e81103b114b68449635900
Leikoe <leo.paille@etu.u-bordeaux.fr>
2023-09-02 03:22:03 +0200

    fix: speed

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
 use std::collections::{HashMap, HashSet};
 use std::fs;
-use std::thread::sleep;
-use std::time::{Duration, Instant};
-use miniz_oxide::deflate::compress_to_vec;
-use indicatif::{ProgressIterator, ParallelProgressIterator, ProgressBar, ProgressStyle};
-use rayon::iter::{ParallelIterator, IntoParallelRefIterator};
+use std::sync::Mutex;
+use std::time::Instant;
+use indicatif::{ProgressIterator, ProgressStyle};
+use miniz_oxide::deflate::core::{compress, CompressorOxide, TDEFLFlush};
+use rayon::iter::ParallelIterator;
 use rayon::prelude::IntoParallelIterator;
+use rand::prelude::*;
+use rayon::current_thread_index;
+use once_cell::sync::Lazy;
 
+const N_CTX: usize = 16;
+const BATCH_SIZE: usize = 1;
+const N_STEPS: usize = 1_000;
+
+static BUFFERS: Lazy<[Mutex<Box<[u8; 100]>>; 32]> = Lazy::new(|| {
+    core::array::from_fn(|_| Mutex::new(Box::new([0u8; 100])))
+});
+static COMPRESSORS: Lazy<[Mutex<Box<CompressorOxide>>; 32]> = Lazy::new(|| {
+    core::array::from_fn(|_| Mutex::new(Box::new(CompressorOxide::default())))
+});
 
 fn main() {
+    Lazy::force(&BUFFERS);
+    Lazy::force(&COMPRESSORS);
+
     let start = Instant::now();
     let text = fs::read_to_string("input.txt").unwrap()[..1000].to_string();
 
@@ -35,43 +51,39 @@ fn main() {
 
     let data = encode(text.as_str());
     let n_vocab: i64 = chars.len() as i64;
-    let n_ctx : i64 = 8;
     let n_train: i64 = data.len() as i64;
 
     println!("n_vocab = {n_vocab}");
-    println!("n_ctx   = {n_ctx}");
+    println!("n_ctx   = {N_CTX}");
     println!("n_train = {n_train}");
 
 
-    let mut X = Vec::with_capacity((n_train*n_ctx) as usize);
-    let mut Y= Vec::with_capacity((n_train*n_ctx) as usize);
+    let mut X = Vec::new();
+    let mut Y= Vec::new();
 
     let before = Instant::now();
-    let d = get_data(&data, n_ctx);
-    println!("get_data | Elapsed time: {:.2?}", before.elapsed());
-
-    let before = Instant::now();
-    for (x, y) in d.0.iter().zip(d.1.iter()).progress() {
-        for token_idx in 0..n_ctx as usize {
-            let context = &x[..token_idx+1];
-            let target = &y[token_idx];
+    for i in (0..N_STEPS).progress() {
+        let (x, y) = get_data(&data);
+        for token_idx in 0..N_CTX {
+            let context = &x[0][..token_idx+1];
+            let target = &y[0][token_idx];
 
             // println!("when context is {:?}, target is {}", decode(context.to_vec()), itos.get(target).unwrap());
-            X.push((context, compress_to_vec(context, 6).len() as i64));
+            X.push((context, compressed_size(context)));
             Y.push(target);
         }
     }
-    println!("X, Y | Elapsed time: {:.2?}", before.elapsed());
+    println!("Getting X,Y | Elapsed time: {:.2?}", before.elapsed());
 
     let before = Instant::now();
     let progress_style = ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {per_sec} {pos:>7}/{len:7} {msg}")
         .unwrap();
     let ncd_scores = (0..X.len())
         .into_iter() // TODO: why is it this slow ? parallel is even slower
-        .progress_count(X.len() as u64)
+        .progress()
         .with_style(progress_style)
         .map(|i| (0..X.len())
-            .into_iter()
+            .into_par_iter()
             .map(|j: usize| ncd(X[i].0, X[i].1, X[j].0, X[j].1))
                 .collect::<Vec<f64>>())
         .collect::<Vec<Vec<f64>>>();
@@ -81,10 +93,12 @@ fn main() {
     println!("TOOK: {}", start.elapsed().as_secs_f64());
 }
 
-fn get_data(data: &Vec<u8>, n_ctx: i64) -> (Vec<&[u8]>, Vec<&[u8]>) {
-    let ix = Vec::from_iter(0..(data.len() - n_ctx as usize));
-    let x = ix.par_iter().map(|&i| &data[i..(i+n_ctx as usize)]).collect();
-    let y = ix.par_iter().map(|&i| &data[(i+1)..(i+n_ctx as usize+1)]).collect();
+fn get_data(data: &Vec<u8>) -> ([&[u8]; BATCH_SIZE], [&[u8]; BATCH_SIZE]) {
+    let max_idx = data.len() - N_CTX;
+    let ix: [usize; BATCH_SIZE] = core::array::from_fn(|x| (random::<f32>() * max_idx as f32) as usize);
+
+    let x: [&[u8]; BATCH_SIZE] = ix.map(|i| &data[i..(i+N_CTX)]);
+    let y: [&[u8]; BATCH_SIZE] = ix.map(|i| &data[(i+1)..(i+N_CTX+1)]);
 
     (x, y)
 }
@@ -102,8 +116,18 @@ fn concat_with_space(x: &[u8], y: &[u8]) -> Vec<u8> {
     s
 }
 
-fn ncd(x: &[u8], x_compressed: i64, x2: &[u8], x2_compressed: i64) -> f64 {
-    let xx2 = compress_to_vec(&*[x, x2].concat(), 6).len() as i64;
+fn compressed_size(x: &[u8]) -> u64 {
+    let thread_id = current_thread_index().unwrap_or(0);
+    let mut buffer = BUFFERS[thread_id].lock().unwrap();
+    let mut compressor = COMPRESSORS[thread_id].lock().unwrap();
+    compressor.reset();
+    let (_, consumed, produced) = compress(&mut compressor, x, &mut buffer[..], TDEFLFlush::None);
+
+    produced as u64
+}
+
+fn ncd(x: &[u8], x_compressed: u64, x2: &[u8], x2_compressed: u64) -> f64 {
+    let xx2 = compressed_size(&concat_with_space(x, x2)[..]);
     (xx2 - x_compressed.min(x2_compressed)) as f64 / x_compressed.max(x2_compressed) as f64
 }
 
